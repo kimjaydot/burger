e7c631d2b5dc69818a30744959b1271e93b14853
Kimberley Johnson <kimberleyjohnson@Kimberleys-MacBook-Pro.local>
2017-06-09 20:34:56 -0500

    create colums

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,9 @@ USE burgers_db;
 
 CREATE TABLE IF NOT NOT EXISTS burgers (
 
-item INT (10) AUTO_INCREMENT NOT NULL,
+id INT (10) AUTO_INCREMENT NOT NULL,
 burger_name VARCHAR(255) NOT NULL,
 devoured VARCHAR(255) NOT NULL,
-date DECIMAL (10, 2) NOT NULL,
-stock_quantity INT(30) NOT NULL, 
-PRIMARY KEY(item_id)
+date TIMESTAMP  NOT NULL,
+PRIMARY KEY(id)
+);
