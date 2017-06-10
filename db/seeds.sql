561362fe619d33aa1ba17483e0d820ef07c4ffaa
Kimberley Johnson <kimberleyjohnson@Kimberleys-MBP.attlocal.net>
2017-06-10 08:40:02 -0500

    create columns

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+
+INSERT INTO pets (id, burger_name, devoured, date ) VALUES ('wolf', 'Cassie', 195, 1);
+INSERT INTO pets (id, burger_name, devoured, date) VALUES ('bear', 'Rachel', 745, 1);
+INSERT INTO pets (id, burger_name, devoured, date) VALUES ('hawk', 'Tobias', 850, 2);
+
