7e9ab1002a02ccfac3e893657cc37c446642a99e
Kimberley Johnson <kimberleyjohnson@Kimberleys-MacBook-Pro.local>
2017-06-09 20:31:13 -0500

    add schema, create database, make table

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS burgers_db
+USE burgers_db;
+
+CREATE TABLE IF NOT NOT EXISTS burgers (
+
+item INT (10) AUTO_INCREMENT NOT NULL,
+burger_name VARCHAR(255) NOT NULL,
+devoured VARCHAR(255) NOT NULL,
+date DECIMAL (10, 2) NOT NULL,
+stock_quantity INT(30) NOT NULL, 
+PRIMARY KEY(item_id)
