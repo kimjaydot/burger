cb5b18be0e5b6ffad1a3fe6713ecc8acbce500df
Kimberley Johnson <kimberleyjohnson@Kimberleys-MBP.attlocal.net>
2017-06-11 11:17:20 -0500

    removed extra commas

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 
 INSERT INTO burgers (burger_name, devoured, date) VALUES ('Shroom Burger', true,);
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Everything is Bigger', true,);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Everything is Bigger', true);
 INSERT INTO burgers (burger_name, devoured, date) VALUES ('Magical', true, );
