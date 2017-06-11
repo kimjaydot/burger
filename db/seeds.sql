848762dae9cd33084f4d049a5891437056e5c2d1
Kimberley Johnson <kimberleyjohnson@Kimberleys-MBP.attlocal.net>
2017-06-11 11:25:37 -0500

    change date to dates

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Shroom Burger', true,);
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Everything is Bigger', true);
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Magical', true, );
+INSERT INTO burgers (burger_name, devoured, dates) VALUES ('Shroom Burger', true);
+INSERT INTO burgers (burger_name, devoured, dates) VALUES ('Everything is Bigger', true);
+INSERT INTO burgers (burger_name, devoured, dates) VALUES ('Magical', true );
