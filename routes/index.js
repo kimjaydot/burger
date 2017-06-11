079531dece8573cf6c08727e5cbfefe3d79c8f6d
Kimberley Johnson <kimberleyjohnson@Kimberleys-MBP.attlocal.net>
2017-06-11 15:59:25 -0500

    updated title name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Test' });
+  res.render('index', { title: 'WTF Burger' });
 });
 
 module.exports = router;
